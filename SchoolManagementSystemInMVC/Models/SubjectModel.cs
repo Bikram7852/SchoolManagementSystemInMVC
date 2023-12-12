567293b20cc2a923bfa38701a4968ad8046b416f
Bikram7852 <bikram.roy7852@gmail.com>
2023-12-12 12:12:15 +0530

    Add project files.

---
+++ SchoolManagementSystemInMVC/Models/SubjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagementSystemInMVC.Models
+{
+    public class SubjectModel
+    {
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
